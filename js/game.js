b04661b7f69864995d495d35c16d50e413583aec
Higor Rodrigues <higor_1992@hotmail.com>
2022-06-25 00:08:39 -0300

    adicionando cards e mecanicas de rodalas

---
+++ js/game.js
@@ -0,0 +1,116 @@
+const grid = document.querySelector('.grid');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disable-card');
+
+    if(disabledCards.leght === 20){
+        alert('Parabéns, você conseguiu !');
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if (firstCharacter === secondCharacter) {
+
+        firstCard.firstChild.classList.add('disable-card');
+        secondCard.firstChild.classList.add('disable-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+
+    } else {
+
+        setTimeout(() => {
+
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+
+        }, 500);
+
+    }
+}
+
+const revealCard = ({ target }) => {
+
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+    if (firstCard === '') {
+
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    } else if (secondCard === "") {
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+
+
+}
+
+const createCard = (character) => {
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../images/${character}.png')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+    card.setAttribute('data-character', character)
+
+    return card;
+}
+
+const loadGame = () => {
+
+    const duplicateCharacters = [...characters, ...characters];
+
+    const shufflerdArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+
+    shufflerdArray.forEach((character) => {
+
+        const card = createCard(character);
+        grid.appendChild(card);
+    });
+
+}
+
+loadGame();
+
